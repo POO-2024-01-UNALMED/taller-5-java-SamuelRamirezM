aa69a0ea04c5abead63f5be29a1e85d8bc881576
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-03-18 20:58:20 -0500

    Zona y Zoológico

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Zoologico {
 	private String nombre;
 	private String ubicacion;
-	private ArrayList zonas;
+	private ArrayList<Zona> zonas = new ArrayList<>();
 	
 	public Zoologico() {
 		this(null, null);
@@ -14,4 +14,41 @@ public class Zoologico {
 		this.nombre = nombre;
 		this.ubicacion = ubicacion;
 	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+     
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+	
+	public ArrayList<Zona> getZona() {
+		return zonas;
+	}
+	
+	public void agregarZonas(Zona zona) {
+		zonas.add(zona);	
+	}
+	
+	public int cantidadTotalAnimales() {
+		int totalAnimales = 0;
+		for (Zona zonas : zonas) {
+			totalAnimales += zonas.cantidadAnimales();
+		}
+		return totalAnimales;
+	}
+
 }
