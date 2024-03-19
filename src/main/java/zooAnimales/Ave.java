5f6b5376af50e7e686a8d7631105a44b72716bf2
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-03-18 19:41:01 -0500

    Clase Ave

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -8,12 +8,20 @@ public class Ave extends Animal{
 	private String colorPlumas;
 	
 	public Ave() {
-		this(0, 0, null);
+		this(null, 0, null, null, null);
 	}
 	
-	public Ave(int halcones, int aguilas, String colorPlumas) {
-		this.halcones = halcones;
-		this.aguilas = aguilas;
-		this.colorPlumas = colorPlumas;
+	public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+		super(nombre,edad,habitat,genero);
+		this.colorPlumas=colorPlumas;
+		Ave.listado.add(this);
 	}
+	
+	public static void setListado(ArrayList<Ave> listado) {
+		Ave.listado=listado;
+	}
+	public static ArrayList<Ave> getListado() {
+		return listado;
+	}
+	
 }
