2b636d4f65a1a932983238a1ac3e925f26a1c20b
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-03-18 20:16:45 -0500

    Clase Pez

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -9,13 +9,58 @@ public class Pez extends Animal{
 	private int cantidadAletas;
 	
 	public Pez() {
-		this(null, 0, 0, null, 0);
+		this(null, 0, null, null, null, 0);
 	}
 	
-	public Pez(int salmones, int bacalaos, String colorEscamas, int cantidadAletas) {
-		this.salmones = salmones;
-		this.bacalaos = bacalaos;
+	public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas) {
+		super(nombre, edad, habitat, genero);
 		this.colorEscamas = colorEscamas;
 		this.cantidadAletas = cantidadAletas;
+		Pez.listado.add(this);
 	}
+	
+	public static void setListado(ArrayList<Pez> listado) {
+		Pez.listado = listado;
+	}
+	
+	public static ArrayList<Pez> getListado() {
+		return listado;
+	}
+	
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+	
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	
+	public void setCantidadAletas(int cantidadAletas) {
+		this.cantidadAletas = cantidadAletas;
+	}
+	
+	public int getCantidadAletas() {
+		return cantidadAletas;
+	}
+	
+	public int cantidadPeces() {
+		return Pez.listado.size();
+	}
+	
+	public String movimiento() {
+		return "nadar";
+	}
+	
+	public static Pez crearSalmon(String nombre, int edad, String genero) {
+		Pez Pez = new Pez(nombre, edad, "oceano", genero, "rojo", 6);
+		salmones++;
+		return Pez;
+	}
+	
+	public static Pez crearBacalao(String nombre, int edad, String genero) {
+		Pez Pez = new Pez(nombre, edad, "oceano", genero, "gris", 6);
+		bacalaos++;
+		return Pez;
+	}
+	
 }
